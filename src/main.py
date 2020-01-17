2ccd20a56cd1470c4fff483c70ea9d11085e2ff7
tsalinas945 <tsalinas2jz@GMAIL.COM>
2020-01-17 01:56:24 +0000

    flask2

--- src/main.py
+++ src/main.py
@@ -7,8 +7,8 @@ from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
-from models import db
-#from models import Person
+from models import db, Person
+
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -28,14 +28,60 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@app.route('/person', methods=['GET', 'POST'])
+def handle_person():
+    if request.method == 'POST':
+        body = request.get_json()  # get the request body content
 
-    response_body = {
-        "hello": "world"
-    }
+        # POST request
+        if body is None:
+            return "Specify body"
+        if 'username' not in body:
+            return 'You need to specify the first_name',400
+        if 'email' not in body:
+            return 'You need to specify the last_name', 400
+        if 'address' not in body:
+            body["address"]= None    
+           
 
-    return jsonify(response_body), 200
+        contact1 = Person(username = body['username'], email = body['email'], address = body['address'])
+        db.session.add(contact1)
+        db.session.commit()
+
+        return "ok", 200
+    
+    if request.method == 'GET':
+
+        people_query = Person.query.all()
+        all_people = list(map(lambda x: x.serialize(), people_query))
+        return jsonify(all_people),200
+
+        
+@app.route('/person/<int:person_id>', methods=['PUT', 'GET'])
+def get_single_person(person_id):
+    """
+    Single person
+    """
+    body = request.get_json() #{ 'username': 'new_username'}
+    if request.method == 'PUT':
+        user1 = Person.query.get(person_id)
+        user1.username = body.username
+        db.session.commit()
+        return jsonify(user1.serialize()), 200
+    if request.method == 'GET':
+        user1 = Person.query.get(person_id)
+        return jsonify(user1.serialize()), 200
+
+    return "Invalid Method", 404
+
+# @app.route('/hello', methods=['POST', 'GET'])
+# def handle_hello():
+
+#     response_body = {
+#         "hello": "world"
+#     }
+
+#     return jsonify(response_body), 200
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
